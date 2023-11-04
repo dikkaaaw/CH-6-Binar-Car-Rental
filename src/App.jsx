9a6ce8ed1b5524fe8f450aafbbb6c6acd75be962
Dika Wicaksono <dhkwck00@gmail.com>
2023-11-04 22:07:03 +0700

    add router in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import "./App.css";
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./pages/LandingPage";
+import ListCars from "./pages/ListCars";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import NoMatch from "./components/NoMatch";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/cars" element={<ListCars />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
